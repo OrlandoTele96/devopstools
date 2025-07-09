2b40da3f501b17f10eac2a862b35fb1f90bb1b43
OrlandoTele96 <jorge.o.gonzalez.dev@gmail.com>
2025-07-08 20:14:11 -0600

    Modifying message

--- scripts/get-argo-password.sh
+++ scripts/get-argo-password.sh
@@ -4,4 +4,4 @@ KUBECONFIG="/home/orlandog/jorgegonzalez-k8s-nyc3-jogg-services-709804ab-kubecon
 
 ARGOCD_ADMIN_PASSWD=$(kubectl --kubeconfig=$KUBECONFIG -n argocd get secret argocd-initial-admin-secret -o=jsonpath='{.data.password}' | base64 -d)
 
-echo "You super admin access password : $ARGOCD_ADMIN_PASSWD"
+echo "You super admin access password to argo cd: $ARGOCD_ADMIN_PASSWD"
